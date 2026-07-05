50b880d4eaec4c54bc15558599b0b5e3dbc97d71
Saurabh98719 <ksaurabh98719@gmail.com>
2026-07-05 13:06:21 +0530

    Add files via upload

---
+++ back.cpp
@@ -0,0 +1,28 @@
+// // #include<iostream>
+// // #include<vector>
+// // using namespace std;
+// // int main(){
+// //     vector<int>vec;
+// //     vec.push_back(2);
+// //     vec.push_back(3);
+// //     vec.push_back(7);
+// //     for(int val: vec){
+// //     cout<<val<<" ";
+// // }
+// // cout<<vec.back()<<endl;
+// // }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int>vec;
+//     vec.push_back(2);
+//     vec.push_back(3);
+//     vec.push_back(7);
+//     for(int val: vec){
+//     cout<<val<<" ";
+// }
+// cout<<"front"<<vec.front()<<" ";
+// cout<<"back"<<vec.back();
+// }
---
+++ clear.cpp
@@ -0,0 +1,11 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int>vec = {2,5,9,5};
+//     vec.clear();
+//     for(int val : vec){
+//         cout<<val<<" ";
+//     }
+// }
+
