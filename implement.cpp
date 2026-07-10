2d41c406f98df8dca3c0b674ca598f79f6ad9ba3
Saurabh98719 <ksaurabh98719@gmail.com>
2026-07-10 14:25:48 +0530

    Add files via upload

--- implement.cpp
+++ implement.cpp
@@ -1,11 +1,13 @@
-// #include<iostream>
-// #include<deque>
-// #include<vector>
-// #include<list>
-// using namespace std;
-// int main(){
-// pair<int,int>p = {3,4};
-// cout<<p.first<<" ";
-// cout<<p.second<<endl;
-// }
-
+#include<iostream>
+#include<map>
+using namespace std;
+int main (){
+    map<string,int>m;
+    m["tv"] = 100000;
+    m["mobile"] = 5000;
+    m["laptop"] = 40000;
+    for(auto val: m){
+        cout<<val.first<<" ";
+        cout<<val.second<< endl;
+    }
+}
