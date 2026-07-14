eed8d0d5d2181b868eba35b9805ddfd3486efc7b
Saurabh98719 <ksaurabh98719@gmail.com>
2026-07-14 13:47:11 +0530

    Add files via upload

---
+++ arraylist.cpp
@@ -0,0 +1,10 @@
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// int main(){
+//     list<int>l = {2, 5, 8,3};
+    
+// for(int val : l){
+//         cout<<val<<" ";
+//     }
+// }
